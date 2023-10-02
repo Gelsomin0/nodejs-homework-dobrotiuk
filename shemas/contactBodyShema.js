c5925a90928e9251898626366a5105a68a64596a
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-10-02 18:18:02 +0300

    Validation is done

--- shemas/contactBodyShema.js
+++ shemas/contactBodyShema.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 const bodyContactSchema = Joi.object({
-  name: Joi.string().required().messages({
+  name: Joi.string().min(3).max(50).required().messages({
     "any.required": "missing required name field"
   }),
-  email: Joi.string().required().messages({
+  email: Joi.string().min(5).max(255).required().email().messages({
     "any.required": "missing required email field"
   }),
-  phone: Joi.string().required().messages({
-    "any.required": "missing required phone field"
+  phone: Joi.string().regex(/^[0-9\-\+]{9,15}$/).required().messages({
+    "any.required": "missing required phone field",
   }),
 });
 
